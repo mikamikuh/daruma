8efaca30702d5e70f814262a03cae33ee0f9ef6a
Kouhei Mikami <mkouhei0910@gmail.com>
2012-01-09 02:52:14 +0900

    Add mongoika to project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject daruma "0.1.0-SNAPSHOT"
             :description "FIXME: write this!"
             :dependencies [[org.clojure/clojure "1.3.0"]
+                           [mongoika "0.6.7"]
                            [noir "1.2.1"]]
             :main daruma.server)
 
