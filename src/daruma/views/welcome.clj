f8159d9a6fc079d4b6662d591073eba9288a3837
Kouhei Mikami <mkouhei0910@gmail.com>
2012-01-08 20:45:52 +0900

    first commit

---
+++ src/daruma/views/welcome.clj
@@ -0,0 +1,9 @@
+(ns daruma.views.welcome
+  (:require [daruma.views.common :as common]
+            [noir.content.getting-started])
+  (:use [noir.core :only [defpage]]
+        [hiccup.core :only [html]]))
+
+(defpage "/welcome" []
+         (common/layout
+           [:p "Welcome to daruma"]))
