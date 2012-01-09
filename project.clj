1eef59ca55242d8cc915418e9aac3da113dbdbec
Kouhei Mikami <mkouhei0910@gmail.com>
2012-01-09 22:21:35 +0900

    Added dependency libs.

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
             :description "FIXME: write this!"
             :dependencies [[org.clojure/clojure "1.3.0"]
                            [congomongo "0.1.7"]
+                           [clj-time "0.3.4"]
                            [noir "1.2.1"]]
             :main daruma.server)
-
